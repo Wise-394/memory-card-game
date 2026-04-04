0ea39a432e81d9d99981d20beb7b515104f4cd6f
wise <114738222+Wise-394@users.noreply.github.com>
2026-04-04 22:39:57 +0800

    Add logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,10 @@ import styles from "@/footer.module.css";
 function Footer() {
   return (
     <div className={styles.footer}>
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+        width="24px"
+      />
       <a href="https://www.github.com/wise-394" target="_blank">
         github.com/wise-394
       </a>
